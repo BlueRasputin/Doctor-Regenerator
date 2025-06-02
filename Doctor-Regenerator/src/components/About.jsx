0085635b6506d69aee52cc1142b1726f82493edc
BlueRasputin <barrettjoshrev10@gmail.com>
2025-06-02 01:35:56 -0500

    completed Doctor Bios

--- Doctor-Regenerator/src/components/About.jsx
+++ Doctor-Regenerator/src/components/About.jsx
@@ -11,7 +11,7 @@ function About() {
             <h3>Doctor Who is a beloved show with a long, expansive and, frankly, absurd history. Its full of inside-jokes, references and a TARDIS-full of expanded media, spanning books, audio dramas, movies, spin-off shows, animated series, podcasts, Web shorts, comic-books and, somehow, even more. This kind of backlog is both an enticing thing for someone looking for a fun new fiction to dive into, but can often feel like staring into the Time Vortex, unsure where to begin or know what's essential. 
             <br />
             <br />
-            That's why I made the Doctor Who Regenerator. To hopefully make the show a bit less Wibbly-Wobbly to get into, and start you on the right path for watching the show. 
+            That's why I made the Doctor Who Regenerator. To make the show a bit less Wibbly-Wobbly to get into, and start you on the right path for watching the show. 
             <br />
             <br />
             Thank you for visiting my site, and please let me know if you have any suggestions for how I can improve the site!</h3>
